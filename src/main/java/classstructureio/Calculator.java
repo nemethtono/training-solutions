51cd3dbc990b27283c9b5917c3f7a4974fde477c
nemethtono <nemethtono@gmail>
2020-10-29 12:29:51 +0100

    second commit

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -1,9 +1,19 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in)
         System.out.println("Kérlek adj meg két egész számot");
+        System.out.println("Első szám");
+        int first = scanner.nextInt();
+        System.out.println("második szám");
+        int second = scanner.nextInt();
+
+        System.out.println(first "+" second);
+        System.out.println(first+second);
     }
     }
 }
