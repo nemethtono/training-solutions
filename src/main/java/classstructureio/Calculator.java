0e6798fd20682bd30a76a72990bd0fb528b30384
nemethtono <nemethtono@gmail>
2020-10-29 12:23:20 +0100

    first commit

---
+++ src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,9 @@
+package classstructureio;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Kérlek adj meg két egész számot");
+    }
+    }
+}
