3c20b2579bb3b3c542f184cc03e775ddaea71c8a
nemethtono <nemethtono@gmail>
2020-10-29 12:39:46 +0100

    registration commit

---
+++ src/main/java/classstructureio/Registration.java
@@ -0,0 +1,19 @@
+package classstructureio;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in)
+        System.out.println("adja meg a nevét:");
+        String name = scanner.nextLine();
+
+
+        System.out.println("adja meg e-mail címét:");
+        String mail = scanner.nextLine();
+
+        System.out.println(name);
+        System.out.println(mail);
+    }
+}
